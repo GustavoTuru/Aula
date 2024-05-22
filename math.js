6115b3bf331a9a832d39990978e8a66bce0b13c7
GustavoTuru <guturu@outlook.com>
2024-05-22 17:04:22 -0300

    operações fodase

---
+++ math.js
@@ -0,0 +1,38 @@
+let x = 535 
+
+let y = 40
+
+
+// soma 
+let z = x + y 
+
+// multiplicação
+let vezes = x * y
+
+// dividir
+let dividir = x / y 
+
+// subtração
+let subtração = x - y
+
+let maior = x > y 
+
+let menor = x < y
+
+let emguau = x == y
+
+console.log(`A soma de ${x} + ${y} = ${z}`)
+
+console.log(`A multiplicação de ${x} por ${y} é igual a ${vezes}`)
+
+console.log(`A divisão de ${x} por ${y} é igual a ${dividir}`)
+
+console.log(`A subtração de ${x} por ${y} é igual a ${subtração}`)
+
+console.log(`${x} é maior que  ${y}?  ${maior}`)
+
+console.log(`${x} é menor que  ${y}?  ${menor}`)
+
+console.log(`${x} é emguau a ${y}?  ${emguau}`)
+
+console.log (0 == undefined)
