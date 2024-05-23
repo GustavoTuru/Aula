f02e71238d83f08e1a6aaccaeafc9da4076e09b6
GustavoTuru <guturu@outlook.com>
2024-05-23 10:43:55 -0300

    campo de testes para revisão de conteúdo da aula1

---
+++ revisãoAula1.js
@@ -0,0 +1,18 @@
+// Vou usar aqui como campo de testes para praticar os conceitos que aprendi durante a aula1 
+
+let nome = `Gustavo Marchizelli`
+
+let idade = 19 
+
+const alturaMetros = 1.85
+
+let isHuman = true 
+
+
+
+
+
+
+console.log(`Meu nome é ${nome} tenho ${idade} e minha altura é ${alturaMetros}`)
+
+console.log(`sou humano? ${isHuman}`)
